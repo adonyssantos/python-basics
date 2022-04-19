6e4388ead056f00c883e361ecf81264933a8a90c
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-19 16:30:28 -0400

    Loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+foods = ["Milk", "Sausages", "Bread", "Cheese", "Apples", "Oranges", "Bananas"]
+
+for food in foods:
+    if food == "Sausages":
+        continue
+
+    print(food)
+
+    if food == "Cheese":
+        break
