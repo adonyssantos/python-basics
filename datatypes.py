36ae5425e6b46dfb5d558f1be4d3af4bf53dcf08
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-19 15:31:39 -0400

    Data types

---
+++ datatypes.py
@@ -0,0 +1,37 @@
+# to show the data type of a variable
+print(type(""))
+
+# str
+print("Hello World")
+print("hello world")
+print("HELLO WORLD")
+print("""Hello World""")
+
+# int
+print(1)
+print(10)
+
+# float
+print(1.0)
+print(1.5)
+
+# bool
+print(True)
+print(False)
+
+# list
+print([1, 2, 3])
+print(["a", "b", "c"])
+
+# tuple
+print((1, 2, 3))
+print(("a", "b", "c"))
+
+# set
+
+print({1, 2, 3})
+print({1, 2, 3, 4, 5})
+
+# dict
+print({1: "a", 2: "b", 3: "c"})
+print({1: "a", 2: "b", 3: "c", 4: "d"})
